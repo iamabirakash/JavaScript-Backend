1524e66ee420aaaa96a4e090eeb45c1c51dbe70e
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-28 21:59:10 +0530

    Added the Jokes JSON

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ const express = require('express') // => Common JS
 // import express from 'express'; // => Module JS
 // TO USE THIS WE HAVE TO ADD "type": "module" into package.json
 const app = express();
-// const port = 3000
+const port = 3000
 
 app.get("/",(req,res) => { //REQ RES Callback
     res.send("Hello World");
 })
-app.get("/login",(req,res) => {
-    res.send("<h1>Hello Login</h1>");
+app.get("/jokes",(req,res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A Joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 2,
+            title: 'A Joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 3,
+            title: 'A Joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 4,
+            title: 'A Joke',
+            content: 'This is a joke'
+        },
+        {
+            id: 5,
+            title: 'A Joke',
+            content: 'This is a joke'
+        }
+    ];
+    res.send(jokes);
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening at port ${port}`);
+    console.log(`Serve at https://localhost:${port}`);
 })
