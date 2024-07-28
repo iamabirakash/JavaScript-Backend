9743ed26694dd0db74d1aadfd0fa7656330c87a7
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-28 20:56:50 +0530

    Added /Login

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get("/",(req,res) => { //REQ RES Callback
     res.send("Hello World");
 })
+app.get("/login",(req,res) => {
+    res.send("<h1>Hello Login</h1>");
+})
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);
